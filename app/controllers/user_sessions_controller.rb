ea1ab0530ee53bbb73b265a5295bd2b006398fbd
jibiking <daiki.jibiki@gmail.com>
2022-01-14 17:29:12 +0900

    feat: sorcery導入完了

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,18 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  def create
+    @user = login(params[:user_name], params[:password])
+
+    if @user
+      redirect_back_or_to(:users, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string
+#  role             :integer          default(0), not null
+#  salt             :string
+#  user_name        :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_user_name  (user_name) UNIQUE
+#
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :user_name, uniqueness: true
+end
