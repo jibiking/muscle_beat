6b81038dfa0cba21594bffd7a662362247916f79
jibiking <daiki.jibiki@gmail.com>
2022-02-05 11:04:03 +0900

    feat: ログインなしで遊べるようにした #31

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  skip_before_action :require_login, only: [:index, :new, :create]
+  skip_before_action :require_login, only: %i[new create]
 
   def index
     @users = User.all
