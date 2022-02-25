6764d20d1fcb9127b3b20433e6794b56b50ee401
jibiking <daiki.jibiki@gmail.com>
2022-02-26 07:52:08 +0900

    refactor: scores_controllerのリファクタ完了 #77

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,49 +1,33 @@
 class ScoresController < ApplicationController
   protect_from_forgery
-  before_action :set_score, only: %i[ show edit update destroy ]
+  before_action :set_score, only: %i[ destroy ]
 
-  # GET /scores or /scores.json
   def index
     @scores = Score.all
   end
 
-  # GET /scores/new
-  def new
-    @score = Score.new
-  end
-
-  # POST /scores or /scores.json
   def create
     @score = Score.new(score_params)
 
-    respond_to do |format|
-      if @score.save
-        format.html { redirect_to score_url(@score), notice: "Score was successfully created." }
-        format.json { render :show, status: :created, location: @score }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @score.errors, status: :unprocessable_entity }
-      end
+    if @score.save
+      redirect_to scores_path, notice: "Score was successfully created."
+    else
+      render :new
     end
   end
 
-  # DELETE /scores/1 or /scores/1.json
   def destroy
     @score.destroy
 
-    respond_to do |format|
-      format.html { redirect_to scores_url, notice: "Score was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to scores_url, notice: "Score was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_score
+
+  def set_score
       @score = Score.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def score_params
       params.permit(:score, :user_id, :beat_id)
     end
