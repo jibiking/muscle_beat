b186771149d4a6c6c800029933768eedc23fbca5
jibiking <daiki.jibiki@gmail.com>
2022-03-01 16:52:07 +0900

    bug: ブラウザバックするとslickが動かない現象解決 #87

--- app/javascript/packs/common/common.js
+++ app/javascript/packs/common/common.js
@@ -41,7 +41,10 @@ $(function() {
 
 
 // スライダー
-$(function() {
+document.addEventListener("turbolinks:before-cache", function(){
+	$('.slider.slick-initialized').slick('unslick');
+});
+$(document).on('turbolinks:load', function () {
 	$('.slider').slick({
 		// autoplay: true,//自動的に動き出すか。初期値はfalse。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
