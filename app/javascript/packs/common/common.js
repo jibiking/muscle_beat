6200f6591a788918f296a7f26745f77624332f88
jibiking <daiki.jibiki@gmail.com>
2022-03-01 16:34:41 +0900

    feat: jsファイルwebpacker側に移行 #87

--- app/javascript/packs/common/common.js
+++ app/javascript/packs/common/common.js
@@ -24,3 +24,34 @@ window.deviceMotionRequest = function(){
     alert('この端末ではご利用できません。')
   }
 }
+
+
+// ハンバーガーメニュー
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').fadeToggle(300);
+      } else {
+          $('.globalMenuSp').fadeToggle(300);
+      }
+  });
+});
+
+
+// スライダー
+$(function() {
+	$('.slider').slick({
+		// autoplay: true,//自動的に動き出すか。初期値はfalse。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		speed: 300,//スライドのスピード。初期値は300。
+		slidesToShow: 3,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		centerMode: true,//要素を中央ぞろえにする
+		variableWidth: true,//幅の違う画像の高さを揃えて表示
+    verticalSwiping: true,//垂直スワイプ
+	});
+});
