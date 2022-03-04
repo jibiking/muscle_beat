90a270a954ce1d68b0db372ffd95befc6bd08029
jibiking <daiki.jibiki@gmail.com>
2022-03-04 12:19:17 +0900

    feat: ユーザーネームを４文字以内に制限 #97

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :user_name, uniqueness: true
+  validates :user_name, uniqueness: true, length: { maximum: 4 }
 
   enum role: { general: 0, admin: 1 }
 
