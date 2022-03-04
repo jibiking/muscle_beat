88614272e3c03e6135e31b49a251f2bfa9bb9a69
jibiking <daiki.jibiki@gmail.com>
2022-03-04 22:23:48 +0900

    feat: explainページの追加＆画像変更 #103

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   resources :scores, only: %i[index create destroy]
   resources :mypage, only: %i[index]
 
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => 'user_sessions#create'
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'explain', to: 'starts#explain'
+
+  get 'login', to: 'user_sessions#new', :as => :login
+  post 'login', to: 'user_sessions#create'
+  post 'logout', to: 'user_sessions#destroy', :as => :logout
 end
