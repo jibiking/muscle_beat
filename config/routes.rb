8d9a22b6ce68679252c8802ae823c57a1f17beab
jibiking <daiki.jibiki@gmail.com>
2022-03-08 16:04:46 +0900

    refactor: 不要なranking関係の削除 #114

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   root :to => 'starts#index'
   resources :starts, only: %i[index]
   resources :users
-  resources :beats do
-    member do
-      resources :ranking, only: %i[index]
-    end
-  end
+  resources :beats
   resources :scores, only: %i[index create destroy]
   resources :mypage, only: %i[index]
 
