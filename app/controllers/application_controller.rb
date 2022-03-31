b912d8edc9af3a97d38e74ad9c31992adf5a6369
jibiking <daiki.jibiki@gmail.com>
2022-03-31 10:34:38 +0900

    feat: 認可に失敗した際のリダイレクト設定 #135

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   # before_action :check_admin
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
     redirect_to login_path, notice: 'ログインが必要です！'
   end
 
-  # def check_admin
-  #   redirect_to beats_path, notice: '権限がありません' unless current_user.admin?
-  # end
+  def user_not_authorized
+    redirect_to beats_path, notice: '権限がありません'
+  end
 end
