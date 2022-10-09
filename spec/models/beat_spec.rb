8b023140a43c916765a68f26b2783870056491b2
jibiking <daiki.jibiki@gmail.com>
2022-10-09 18:32:17 +0900

    create: docker化完了

--- spec/models/beat_spec.rb
+++ spec/models/beat_spec.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: beats
+#
+#  id                   :bigint           not null, primary key
+#  gif                  :string           not null
+#  level                :integer          not null
+#  music                :string           not null
+#  notes                :string           not null
+#  thumbnail            :string           not null
+#  title                :string           not null
+#  training_description :string           not null
+#  training_muscle      :string           not null
+#  url                  :string           not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  user_id              :bigint           not null
+#
+# Indexes
+#
+#  index_beats_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Beat, type: :model do
